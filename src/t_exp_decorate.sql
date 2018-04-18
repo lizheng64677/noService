fafe31047416de2fd2c7d6c8fb0e6154e85b2b33
system9@163.com <qq329009099>
2018-04-18 23:17:51 +0800

    新增字段
    新增字段

--- src/t_exp_decorate.sql
+++ src/t_exp_decorate.sql
@@ -1,7 +1,7 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : 本地
+Source Server         : localhost
 Source Server Version : 50520
 Source Host           : localhost:3306
 Source Database       : db_no
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50520
 File Encoding         : 65001
 
-Date: 2018-04-18 17:59:29
+Date: 2018-04-18 23:17:08
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -23,6 +23,7 @@ CREATE TABLE `t_exp_decorate` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(254) DEFAULT NULL COMMENT '活动名称',
   `title` varchar(512) DEFAULT NULL COMMENT '活动标题',
+  `status` int(1) DEFAULT '0' COMMENT '活动状态 0 未开始，1开始 2结束',
   `description` text COMMENT '活动描述',
   `begin_time` datetime DEFAULT NULL COMMENT '活动开始时间',
   `end_time` datetime DEFAULT NULL COMMENT '活动结束时间',
@@ -34,10 +35,14 @@ CREATE TABLE `t_exp_decorate` (
   `share_img` varchar(1024) DEFAULT NULL COMMENT '页面分享图片',
   `begin_money` decimal(10,2) DEFAULT '0.00' COMMENT '随机金额开始数目',
   `end_money` decimal(10,2) DEFAULT '0.00' COMMENT '随机金额结束数目',
+  `exp_uv` int(11) DEFAULT NULL COMMENT '参与人数',
+  `exp_pv` int(11) DEFAULT NULL COMMENT '访问次数',
+  `count_price` decimal(10,2) DEFAULT NULL COMMENT '累积金额',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of t_exp_decorate
 -- ----------------------------
-INSERT INTO `t_exp_decorate` VALUES ('1', null, null, null, null, null, null, null, null, null, null, null, '1.23', null);
+INSERT INTO `t_exp_decorate` VALUES ('9', '111', '11', '0', '22222222222222', '2018-04-10 21:31:00', '2018-05-04 21:31:00', '2018-04-18 21:31:34', '2018-04-18 21:31:34', null, null, '234', '/noService/resources/outImages/decorate/20180418/20180418213132_957.png', '1.00', '2.00', '1', '1002', '12834.32');
+INSERT INTO `t_exp_decorate` VALUES ('10', '535', '345', '1', '333', '2018-04-10 21:56:00', '2018-05-04 21:56:00', '2018-04-18 21:57:11', '2018-04-18 21:57:11', null, null, '33', '/noService/resources/outImages/decorate/20180418/20180418215709_680.png', '1.00', '2.00', '23', '234234', '53223.63');
